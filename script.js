ce0770b498f004ac7b821adfaff00e83dcac8463
ggabrielaoliveira <gabi.oliveiraa@yahoo.com>
2022-08-15 22:09:03 -0300

    Exercicio da aula

---
+++ script.js
@@ -0,0 +1,30 @@
+// // exercicio 1
+// let usuario= prompt("Qual seu tipo de usuario\nA)Administrador\nB)Usuario comun\nC)Usuario assinante").toUpperCase()
+
+// while(usuario !== "A"){
+//     alert("Acesso negado")
+// }
+// alert("Boas vindas, Admin!");
+
+// exercicio 2
+// let num
+// // =Number(prompt("Digite um numero onde a tabuada parará"))
+// for(let i=0;i<=10;i++){
+//     num= i*2
+//     console.log(num)
+// }
+// CODIGO TULIO
+let num=Number(prompt("digite ate quando vc quer que vá a tabuada do 2"));
+for(let i=1; i<=num; i++){
+console.log(`2x${i}=${2*i}`)
+}
+// 
+// 
+
+// // exercicio 3
+// let nomesArray=["andressa","bruna","cristine"]
+// for(let i=0;i < nomesArray.length;i++){
+// console.log(nomesArray[i].toUpperCase())
+// }
+
+
